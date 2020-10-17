99cea3902640ee557bdd8c9fdf1e84e0b9f56aec
Mox <moxalibudbud@gmail.com>
2020-10-17 02:26:58 -0700

    Prepare modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '@modules/user/user.module';
+import { ProductModule } from '@modules/product/product.module';
+import { OrderModule } from '@modules/order/order.module';
+import { CartModule } from '@modules/cart/cart.module';
 
 @Module({
   imports: [
   	MongooseModule.forRoot(process.env.DB_CONNECTION),
-  	UserModule
+  	UserModule,
+  	ProductModule,
+  	OrderModule,
+  	CartModule
   ]
 })
 export class AppModule {}
