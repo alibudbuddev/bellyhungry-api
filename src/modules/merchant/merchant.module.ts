1042ab106d9613db43d435882e991b547ccfe7da
Mox <moxalibudbud@gmail.com>
2020-10-31 03:51:59 -0700

    Create merchant service

--- src/modules/merchant/merchant.module.ts
+++ src/modules/merchant/merchant.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MerchantController } from './merchant.controller';
 import { OrderModule } from '@modules/order/order.module';
 import { OrderItem, OrderItemSchema } from '@schema/order-items.schema';
+import { MerchantService } from './merchant.service';
 
 @Module({
   controllers: [MerchantController],
-  imports: [OrderModule]
+  imports: [OrderModule],
+  providers: [MerchantService]
 })
 export class MerchantModule {}
